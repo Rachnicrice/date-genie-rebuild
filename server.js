4f6a3f873c9ab015874fe9791f75cec75bf11cda
Rachael Rice <Rachnicrice@gmail.com>
2019-11-04 13:56:21 -0800

    Added method override

--- server.js
+++ server.js
@@ -5,16 +5,29 @@ const express = require('express');
 require('dotenv').config();
 const superagent = require('superagent');
 const pg = require('pg');
+const methodOverride = require('method-override');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
 
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(methodOverride((request, response) => {
+  if (request.body && typeof request.body === 'object' && '_method' in request.body) {
+    let method = request.body._method;
+    delete request.body._method;
+    return method;
+  }
+}));
+
 
 //error handlers:
 app.get('*', notFoundHandler);
 app.use(errorHandler);
+
 
 
 function notFoundHandler(req, res) {
