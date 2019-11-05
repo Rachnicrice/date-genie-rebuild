c8c2e92b67756e2b36c02d9200a62edea7bee930
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-11-05 15:37:27 -0800

    fixing pull

--- modules/restauraunts.js
+++ modules/restauraunts.js
@@ -12,7 +12,7 @@ function handleYelp(req, res, locationObj) {
       const yelpData = resultsFromAPI.body.businesses.map(restaurants => {
         return new Restaurant(restaurants);
       });
-      console.log(yelpData);
+      // console.log(yelpData);
       res.status(200).render('pages/searchResults', {resultsArray: yelpData});
 
     })
