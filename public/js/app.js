800e788cd001d7ff2c8778d77480d0a6c189163f
Rachael Rice <Rachnicrice@gmail.com>
2019-11-05 17:11:25 -0800

    Added functionality for users to create new account

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $('nav').hide();
+$('.login').hide();
 
 $('.unhide').on('click', function () {
   console.log(`I'm clicked!`)
@@ -10,3 +11,18 @@ $('.unhide').on('click', function () {
 $('nav > p > a').on('click', function () {
   $('nav').fadeOut(10000);
 })
+
+function authenticateUser () {
+  if (localStorage.getItem('user')) {
+    $.get('/user', localStorage.getItem('user'))
+  } else {
+    $('.login').fadeIn();
+  }
+}
+
+$('#login').on('click', function () {
+  let user = $('.login > input').val();
+  localStorage.setItem('user', user)
+});
+
+authenticateUser();
